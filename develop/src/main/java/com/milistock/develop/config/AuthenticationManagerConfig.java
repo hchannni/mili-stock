6df853d3959542012688a52e8b37d94be421532f
Donghyun Kim <ddoong10@naver.com>
2023-08-24 15:16:47 +0000

    check_point_3

--- develop/src/main/java/com/milistock/develop/config/AuthenticationManagerConfig.java
+++ develop/src/main/java/com/milistock/develop/config/AuthenticationManagerConfig.java
@@ -1,5 +1,27 @@
 package com.milistock.develop.config;
 
-public class AuthenticationManagerConfig {
-    
+import com.milistock.develop.security.jwt.filter.JwtAuthenticationFilter;
+import com.milistock.develop.security.jwt.provider.JwtAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationManagerConfig extends AbstractHttpConfigurer<AuthenticationManagerConfig, HttpSecurity> {
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+        builder.addFilterBefore(
+                new JwtAuthenticationFilter(authenticationManager),
+                UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(jwtAuthenticationProvider);
+    }
 }
--- develop/src/main/java/com/milistock/develop/config/IdentityVerificationInitializer.java
+++ develop/src/main/java/com/milistock/develop/config/IdentityVerificationInitializer.java
@@ -1,7 +1,29 @@
 package com.milistock.develop.config;
 
+import com.milistock.develop.domain.IdentityVerification;
+import com.milistock.develop.repository.IdentityVerificationRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class IdentityVerificationInitializer {
-    
+
+ /*   @Bean
+    public CommandLineRunner initRoles(IdentityVerificationRepository roleRepository) {
+        return args -> {
+            if (roleRepository.count() == 0) { // role 테이블에 데이터가 없을 경우
+                IdentityVerification userRole = new Role();
+                userRole.setRoleId(1L);
+                userRole.setName("ROLE_USER");
+
+                IdentityVerification adminRole = new Role();
+                adminRole.setRoleId(2L);
+                adminRole.setName("ROLE_ADMIN");
+
+                roleRepository.save(userRole);
+                roleRepository.save(adminRole);
+            }
+        };
+    }*/
 }
