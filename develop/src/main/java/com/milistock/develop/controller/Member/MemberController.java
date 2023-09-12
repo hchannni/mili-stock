4451544582254c3628a30f24fe3a765c26b5aeb3
Taeyon Kim <taeyon998@gmail.com>
2023-09-12 11:54:27 +0000

    쓰레기 같지만 어찌 완성한 product API (cart에 member column)

--- develop/src/main/java/com/milistock/develop/controller/Member/MemberController.java
+++ develop/src/main/java/com/milistock/develop/controller/Member/MemberController.java
@@ -1,6 +1,9 @@
 package com.milistock.develop.controller.Member;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,10 @@ public class MemberController {
     @PostMapping
     public Member createMember(@RequestBody Member member) {
         return memberRepository.save(member);
+    }
+
+    @GetMapping("/all")
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
     }
 }
