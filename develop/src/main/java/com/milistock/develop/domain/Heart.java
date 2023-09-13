d6239112714a8573d603092f28723f5f25caa5c6
Taeyon Kim <taeyon998@gmail.com>
2023-09-13 14:06:55 +0000

    heart

--- develop/src/main/java/com/milistock/develop/domain/Heart.java
+++ develop/src/main/java/com/milistock/develop/domain/Heart.java
@@ -1,5 +1,32 @@
 package com.milistock.develop.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="heart")
+@Table(name = "heart")
 public class Heart {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "heart_id")
+    private int heartId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
