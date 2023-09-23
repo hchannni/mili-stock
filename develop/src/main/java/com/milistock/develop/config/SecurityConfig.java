bfe62ea34dc46768f2d5b52051317041f47b0c79
Donghyun Kim <ddoong10@naver.com>
2023-09-23 10:11:18 +0000

    config_setting

--- develop/src/main/java/com/milistock/develop/config/SecurityConfig.java
+++ develop/src/main/java/com/milistock/develop/config/SecurityConfig.java
@@ -25,61 +25,65 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthenticationManagerConfig authenticationManagerConfig;
-    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+        private final AuthenticationManagerConfig authenticationManagerConfig;
+        private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable())
-                .csrf(csrf -> csrf.disable())
-                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
-                        .configurationSource(corsConfigurationSource()))
-                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable())
-                .authorizeHttpRequests(httpRequests -> httpRequests
-                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // Preflight 요청은 허용한다.
-                                                                                    // https://velog.io/@jijang/%EC%82%AC%EC%A0%84-%EC%9A%94%EC%B2%AD-Preflight-request
-                        .mvcMatchers("/members/signup", "/members/login", "/members/refreshToken",
-                                "/members/identity", "/members/help/idInquiry", "/members/help/idCheck",
-                                "/members/help/pwInquiry",
-                                "/members/help/pwChange", "/members/idDuplicate", "/members/all").permitAll()
-                        .mvcMatchers(GET, "/carts/**").hasAnyRole("USER", "ADMIN")
-                        .mvcMatchers(POST, "/carts/**").hasAnyRole("ADMIN")
-                        .mvcMatchers(GET, "/haerts/**").hasAnyRole("USER", "ADMIN")
-                        .mvcMatchers(GET, "/products/**").hasAnyRole("USER", "ADMIN")
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+                httpSecurity
+                                .sessionManagement(
+                                                httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable())
+                                .csrf(csrf -> csrf.disable())
+                                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
+                                                .configurationSource(corsConfigurationSource()))
+                                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable())
+                                .authorizeHttpRequests(httpRequests -> httpRequests
+                                                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // Preflight
+                                                                                                            // 요청은 허용한다.
+                                                                                                            // https://velog.io/@jijang/%EC%82%AC%EC%A0%84-%EC%9A%94%EC%B2%AD-Preflight-request
+                                                .mvcMatchers("/members/signup", "/members/login",
+                                                                "/members/refreshToken",
+                                                                "/members/identity", "/members/help/idInquiry",
+                                                                "/members/help/idCheck",
+                                                                "/members/help/pwInquiry",
+                                                                "/members/help/pwChange", "/members/idDuplicate",
+                                                                "/members/all")
+                                                .permitAll()
+                                                .mvcMatchers(GET, "/carts/**").hasAnyRole("USER", "ADMIN")
+                                                .mvcMatchers(POST, "/carts/**").hasAnyRole("ADMIN")
+                                                .mvcMatchers(GET, "/haerts/**").hasAnyRole("USER", "ADMIN")
+                                                .mvcMatchers(GET, "/products/**").hasAnyRole("USER", "ADMIN")
+                                                .anyRequest().hasAnyRole("USER", "ADMIN"))
+                                .exceptionHandling(
+                                                httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer
+                                                                .authenticationEntryPoint(
+                                                                                customAuthenticationEntryPoint))
+                                .apply(authenticationManagerConfig);
 
-                        // .mvcMatchers(GET, "/categories/**", "/products/**").permitAll()
-                        // .mvcMatchers(GET,"/**").hasAnyRole( "USER")
-                        // .mvcMatchers(POST,"/**").hasAnyRole("USER", "ADMIN")
-                        .anyRequest().hasAnyRole("USER", "ADMIN"))
-                .exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer
-                        .authenticationEntryPoint(customAuthenticationEntryPoint))
-                .apply(authenticationManagerConfig);
+                return httpSecurity.build();
+        }
 
-        return httpSecurity.build();
-    }
+        // <<Advanced>> Security Cors로 변경 시도
+        public CorsConfigurationSource corsConfigurationSource() {
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                CorsConfiguration config = new CorsConfiguration();
+                // config.setAllowCredentials(true); // 이거 빼면 된다
+                // https://gareen.tistory.com/66
+                config.addAllowedOrigin("*");
+                config.addAllowedMethod("*");
+                config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PATCH", "OPTION", "PUT"));
+                source.registerCorsConfiguration("/**", config);
 
-    // <<Advanced>> Security Cors로 변경 시도
-    public CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        // config.setAllowCredentials(true); // 이거 빼면 된다
-        // https://gareen.tistory.com/66
-        config.addAllowedOrigin("*");
-        config.addAllowedMethod("*");
-        config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PATCH", "OPTION", "PUT"));
-        source.registerCorsConfiguration("/**", config);
+                return source;
+        }
 
-        return source;
-    }
-
-    // 암호를 암호화하거나, 사용자가 입력한 암호가 기존 암호랑 일치하는지 검사할 때 이 Bean을 사용
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        // 암호를 암호화하거나, 사용자가 입력한 암호가 기존 암호랑 일치하는지 검사할 때 이 Bean을 사용
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 }
 
 /*
