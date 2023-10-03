2e63f90da761748d2f11da27fed7802daf4dea47
Donghyun Kim <ddoong10@naver.com>
2023-10-03 05:12:12 +0000

    jwt token error handling 수정

--- develop/src/main/java/com/milistock/develop/security/jwt/exception/CustomAuthenticationEntryPoint.java
+++ develop/src/main/java/com/milistock/develop/security/jwt/exception/CustomAuthenticationEntryPoint.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import com.milistock.develop.code.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -23,33 +22,42 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
         String exception = request.getAttribute("exception").toString();
 
-        if(exception == null) {
+            if(exception == null) {
             setResponse(response, JwtExceptionCode.UNKNOWN_ERROR);
-        }
-        else if(exception.equals(JwtExceptionCode.INVALID_TOKEN.getCode())){
-            setResponse(response, JwtExceptionCode.INVALID_TOKEN);
-        }
-        else if(exception.equals(JwtExceptionCode.EXPIRED_TOKEN.getCode())){
-            setResponse(response, JwtExceptionCode.EXPIRED_TOKEN);
-        }
-        else if(exception.equals(JwtExceptionCode.UNSUPPORTED_TOKEN.getCode())){
-            setResponse(response, JwtExceptionCode.UNSUPPORTED_TOKEN);
-        }
-        else if(exception.equals(JwtExceptionCode.NOT_FOUND_TOKEN.getCode())){
-            setResponse(response, JwtExceptionCode.NOT_FOUND_TOKEN);
-        }
-        else {
-            setResponse(response, JwtExceptionCode.UNKNOWN_ERROR);
-        }
+            }
+            //유효하지 않은 타입의 토큰
+            else if (exception.equals(JwtExceptionCode.INVALID_TOKEN.getCode())) {
+                log.error("entry point >> invalid token");
+                setResponse(response, JwtExceptionCode.INVALID_TOKEN);
+            }
+            //토큰 만료된 경우
+            else if (exception.equals(JwtExceptionCode.EXPIRED_TOKEN.getCode())) {
+                log.error("entry point >> expired token");
+                setResponse(response, JwtExceptionCode.EXPIRED_TOKEN);
+            }
+            //지원되지 않는 토큰인 경우
+            else if (exception.equals(JwtExceptionCode.UNSUPPORTED_TOKEN.getCode())) {
+                log.error("entry point >> unsupported token");
+                setResponse(response, JwtExceptionCode.UNSUPPORTED_TOKEN);
+            
+            //토큰 존재하지 않는 경우
+            } else if (exception.equals(JwtExceptionCode.NOT_FOUND_TOKEN.getCode())) {
+                log.error("entry point >> not found token");
+                setResponse(response, JwtExceptionCode.NOT_FOUND_TOKEN);
+
+            } else {
+                setResponse(response, JwtExceptionCode.UNKNOWN_ERROR);
+            }
         
     }
 
     private void setResponse(HttpServletResponse response, JwtExceptionCode exceptionCode) throws IOException {
         
         response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_OK);
 
         HashMap<String, Object> errorInfo = new HashMap<>();
+        errorInfo.put("status", exceptionCode.getStatus());
         errorInfo.put("code", exceptionCode.getCode());
         errorInfo.put("message", exceptionCode.getMessage());
         Gson gson = new Gson();
--- develop/src/main/java/com/milistock/develop/security/jwt/filter/JwtAuthenticationFilter.java
+++ develop/src/main/java/com/milistock/develop/security/jwt/filter/JwtAuthenticationFilter.java
@@ -1,79 +1,72 @@
 package com.milistock.develop.security.jwt.filter;
 
+import java.io.IOException;
 
-import com.milistock.develop.security.jwt.exception.JwtExceptionCode;
-import com.milistock.develop.security.jwt.token.JwtAuthenticationToken;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.IOException;
+import com.milistock.develop.security.jwt.exception.JwtExceptionCode;
+import com.milistock.develop.security.jwt.token.JwtAuthenticationToken;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final AuthenticationManager authenticationManager;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-        String token="";
+            FilterChain filterChain) throws ServletException, IOException {
+        String token = "";
         try {
-            token = getToken(request); //accessToken 받는 메소드
+            token = getToken(request);
             if (StringUtils.hasText(token)) {
-                getAuthentication(token); //Token값이 존재한다면 getAuthentication으로 보내기
+                getAuthentication(token);
             }
-            // filterChain.doFilter(request, response);
-        }
-        catch (NullPointerException | IllegalStateException e) {
+
+        } catch (NullPointerException | IllegalStateException e) {
             request.setAttribute("exception", JwtExceptionCode.NOT_FOUND_TOKEN.getCode());
-            log.error("Not found Token // token : {}", token);
-            log.error("Set Request Exception Code : {}", request.getAttribute("exception"));
+
         } catch (SecurityException | MalformedJwtException e) {
             request.setAttribute("exception", JwtExceptionCode.INVALID_TOKEN.getCode());
-            log.error("Invalid Token // token : {}", token);
-            log.error("Set Request Exception Code : {}", request.getAttribute("exception"));
+
         } catch (ExpiredJwtException e) {
             request.setAttribute("exception", JwtExceptionCode.EXPIRED_TOKEN.getCode());
-            log.error("Set Request Exception Code : {}", request.getAttribute("exception"));
+
         } catch (UnsupportedJwtException e) {
             request.setAttribute("exception", JwtExceptionCode.UNSUPPORTED_TOKEN.getCode());
+            
         } catch (Exception e) {
-            log.error("====================================================");
-            log.error("JwtFilter - doFilterInternal() 오류 발생");
-            log.error("token : {}", token);
-            log.error("Exception Message : {}", e.getMessage());
-            log.error("Exception StackTrace : {");
-            e.printStackTrace();
-            log.error("}");
-            log.error("====================================================");
             request.setAttribute("exception", JwtExceptionCode.UNKNOWN_ERROR.getCode());
         }
         filterChain.doFilter(request, response);
+
     }
 
     private void getAuthentication(String token) {
-        JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(token); //객체생성
+        JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(token); // 객체생성
         Authentication authenticate = authenticationManager.authenticate(authenticationToken); // 객체 분석
-                // 이 객체에는 JWT안의 내용을 가지고 name,role, memberId를 보내줌
+        // 이 객체에는 JWT안의 내용을 가지고 name,role, memberId를 보내줌
 
         SecurityContextHolder.getContext().setAuthentication(authenticate); // 현재 요청에서 언제든지 인증정보를 꺼낼 수 있도록 해준다.
     }
-    //token값 에서 beark 분리
+
+    // token값 에서 beark 분리
     private String getToken(HttpServletRequest request) {
         String authorization = request.getHeader("Authorization");
-        if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer")){
+        if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer")) {
             String[] arr = authorization.split(" ");
             return arr[1];
         }
