b9e1ee0c142577e2119c4869e676b1a26805b5b0
Donghyun Kim <ddoong10@naver.com>
2023-12-08 11:57:22 +0000

    Isheart method and clean code

--- develop/src/main/java/com/milistock/develop/controller/products/ProductController.java
+++ develop/src/main/java/com/milistock/develop/controller/products/ProductController.java
@@ -2,6 +2,7 @@ package com.milistock.develop.controller.products;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -18,8 +19,11 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,27 +31,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.milistock.develop.domain.Product;
 import com.milistock.develop.dto.ProductDto;
-
 import com.milistock.develop.dto.ProductEditDto;
-
+import com.milistock.develop.dto.ProductResponseDto;
 import com.milistock.develop.repository.CartItemRepository;
 import com.milistock.develop.repository.HeartRepository;
 import com.milistock.develop.repository.ProductRepository;
-import com.milistock.develop.service.ProductService;
-
-
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.milistock.develop.response.ValidationResponse;
+import com.milistock.develop.security.jwt.util.IfLogin;
+import com.milistock.develop.security.jwt.util.LoginUserDto;
+import com.milistock.develop.service.ProductService;
 import com.milistock.develop.service.S3UploadService;
-import java.util.Comparator;
-
 
 @RestController
 @RequestMapping("/products")
@@ -74,13 +71,14 @@ public class ProductController {
 
     // 상품 검색 메소드
     @GetMapping("/search")
-    public ResponseEntity<Page<Product>> searchProducts(
+    public ResponseEntity<Page<ProductResponseDto>> searchProducts(
+            @IfLogin LoginUserDto loginUserDto,
             @RequestParam(required = false) String keyword,
             @RequestParam(required = false) String category,
             @RequestParam(required = false) String sortBy,
             @PageableDefault(size = 10) Pageable pageable) {
 
-        Page<Product> results;
+        Page<ProductResponseDto> results;
 
         if (category != null) {
             if (keyword != null) {
@@ -99,23 +97,13 @@ public class ProductController {
 
                 List<Product> resultList = new ArrayList<>(searchResults);
 
-                //sorting method
-                List<Product> sortedList = getSort(resultList, sortBy);
+                results = getHeartAndConverPage(resultList, sortBy, loginUserDto, pageable);
 
-                // 결과 리스트를 페이지로 변환
-                int start = (int) pageable.getOffset();
-                int end = Math.min((start + pageable.getPageSize()), sortedList.size());
-                results = new PageImpl<>(sortedList.subList(start, end), pageable, sortedList.size());
             } else {
                 // 카테고리만 입력된 경우
                 List<Product> resultList = new ArrayList<>(productRepository.findByCategory(category));
 
-                //sorting method
-                List<Product> sortedList = getSort(resultList, sortBy);
-
-                int start = (int) pageable.getOffset();
-                int end = Math.min((start + pageable.getPageSize()), sortedList.size());
-                results = new PageImpl<>(sortedList.subList(start, end), pageable, sortedList.size());
+                results = getHeartAndConverPage(resultList, sortBy, loginUserDto, pageable);
             }
         } else {
             if (keyword != null) {
@@ -130,24 +118,12 @@ public class ProductController {
 
                 List<Product> resultList = new ArrayList<>(searchResults);
 
-                //sorting method
-                List<Product> sortedList = getSort(resultList, sortBy);
-
-                // 결과 리스트를 페이지로 변환
-                int start = (int) pageable.getOffset();
-                int end = Math.min((start + pageable.getPageSize()), sortedList.size());
-                results = new PageImpl<>(sortedList.subList(start, end), pageable, sortedList.size());
+                results = getHeartAndConverPage(resultList, sortBy, loginUserDto, pageable);
             } else {
                 // 아무 입력도 없는 경우
                 List<Product> resultList = productService.getAllProducts();
 
-                //sorting method
-                List<Product> sortedList = getSort(resultList, sortBy);
-
-                // 결과 리스트를 페이지로 변환
-                int start = (int) pageable.getOffset();
-                int end = Math.min((start + pageable.getPageSize()), sortedList.size());
-                results = new PageImpl<>(sortedList.subList(start, end), pageable, sortedList.size());
+                results = getHeartAndConverPage(resultList, sortBy, loginUserDto, pageable);
             }
         }
 
@@ -158,7 +134,7 @@ public class ProductController {
     @PostMapping("/create")
     public ResponseEntity<?> createProduct(@Valid @ModelAttribute ProductDto productDto, BindingResult bindingResult)
             throws IOException {
-        
+
         if (bindingResult.hasErrors()) {
 
             // 유저가 뭘 잘못 입력했는지 출력
@@ -174,7 +150,7 @@ public class ProductController {
         String uploadedUrl = null;
 
         // image 없어도 됨
-        if (image != null){            
+        if (image != null) {
             uploadedUrl = s3UploadService.upload(image);
         }
 
@@ -187,8 +163,8 @@ public class ProductController {
         return ResponseEntity.ok("Success");
 
     }
-      
-      
+
+    // sorting method
     private List<Product> getSort(List<Product> productList, String sortBy) {
         if (sortBy == null) {
             // 기본 정렬 조건 추가
@@ -196,19 +172,19 @@ public class ProductController {
         } else {
             // 사용자가 전달한 정렬 조건에 따라 설정
             switch (sortBy) {
-                case "stockHighToLow": //재고 많은 순
+                case "stockHighToLow": // 재고 많은 순
                     productList.sort(Comparator.comparing(Product::getProductStock).reversed());
                     break;
-                case "stockLowToHigh": //재고 적은 순
+                case "stockLowToHigh": // 재고 적은 순
                     productList.sort(Comparator.comparing(Product::getProductStock));
                     break;
-                case "priceHighToLow": //가격 높은 순
+                case "priceHighToLow": // 가격 높은 순
                     productList.sort(Comparator.comparing(Product::getProductPrice).reversed());
                     break;
-                case "priceLowToHigh": //가격 낮은 순
+                case "priceLowToHigh": // 가격 낮은 순
                     productList.sort(Comparator.comparing(Product::getProductPrice));
                     break;
-                case "newer": //신상품 순
+                case "newer": // 신상품 순
                     productList.sort(Comparator.comparing(Product::getProductTimeAdded).reversed());
                     break;
                 case "popular":
@@ -216,17 +192,41 @@ public class ProductController {
                     productList.sort(Comparator.comparing(Product::getProductHeartCount).reversed());
                     break;
                 default:
-                productList.sort(Comparator.comparing(Product::getProductHeartCount).reversed());
+                    productList.sort(Comparator.comparing(Product::getProductHeartCount).reversed());
             }
         }
         return productList;
+    }
+
+    //isHeart And Convert Page method
+    private Page<ProductResponseDto> getHeartAndConverPage(List<Product> resultList, String sortBy,
+            LoginUserDto loginUserDto, Pageable pageable) {
+
+        // sorting method
+        List<Product> sortedList = getSort(resultList, sortBy);
+
+        // hasHeart method
+        List<ProductResponseDto> heartList = new ArrayList<>();
+
+        for (Product products : sortedList) {
+            boolean isHeart = productService.hasUserLikedProduct(loginUserDto.getMemberId(), products);
+            ProductResponseDto productResponseDto = productService.convertToDto(products);
+            productResponseDto.setIsHeart(isHeart);
+            heartList.add(productResponseDto);
+        }
+
+        // 결과 리스트를 페이지로 변환
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), heartList.size());
+        Page<ProductResponseDto> result = new PageImpl<>(heartList.subList(start, end), pageable, heartList.size());
+
+        return result;
     }
 
     @GetMapping("/all")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
-
 
     // 상품 수정 메소드
     @PutMapping("/edit")
@@ -246,16 +246,15 @@ public class ProductController {
         }
     }
 
-    //신상품 업데이트 메소드
+    // 신상품 업데이트 메소드
     @PutMapping("/updateNewProduct")
     public ResponseEntity<?> updateNewProduct() {
 
         productService.updateProductStatus();
 
         return ResponseEntity.ok("신상품이 업데이트 되었습니다.");
-        
-    }
 
+    }
 
     // 상품 삭제 메소드
     @Transactional
@@ -264,12 +263,11 @@ public class ProductController {
         System.out.println("before getProductById");
         Product product = productService.getProductById(productNumber); // exception 처리 함
 
-
-        try{
-            if(cartItemRepository.existsByProduct(product)){
+        try {
+            if (cartItemRepository.existsByProduct(product)) {
                 cartItemRepository.deleteByProduct(product);
             }
-            if(heartRepository.existsByProduct(product)){
+            if (heartRepository.existsByProduct(product)) {
                 heartRepository.deleteByProduct(product);
             }
 
@@ -277,33 +275,32 @@ public class ProductController {
 
             String successResponse = "상품id= " + productNumber + "가 삭제되었습니다";
             return new ResponseEntity<String>(successResponse, HttpStatus.OK);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 
-    //신상품 조회 메소드
+    // 신상품 조회 메소드
     @GetMapping("/newProduct")
     public ResponseEntity<?> newProducts() {
         List<Product> newProducts = productRepository.findByIsNewProduct(true);
         return ResponseEntity.ok(newProducts);
     }
 
-    //할인상품 조회 메소드
+    // 할인상품 조회 메소드
     @GetMapping("/discountProduct")
     public ResponseEntity<?> discountProducts() {
         List<Product> discountProducts = productRepository.findByIsDiscountedProduct(true);
         return ResponseEntity.ok(discountProducts);
     }
 
-    //인기상품 조회 메소드
+    // 인기상품 조회 메소드
     @GetMapping("/popularProduct")
     public ResponseEntity<?> popularProducts() {
         List<Product> popularProducts = productRepository.findByIsPopularProduct(true);
         return ResponseEntity.ok(popularProducts);
     }
-    
+
     @GetMapping
     public ResponseEntity<List<Product>> getProducts(Pageable pageable) {
         Page<Product> productPage = productService.getAllProducts(pageable);
